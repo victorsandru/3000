eddfa2fc2c9785e6e1ff918619c62f53d8ad5370
Victor Sandru <vsandru4@gmail.com>
2023-10-06 12:31:24 -0400

    added t2

---
+++ t/t2/hello.s
@@ -0,0 +1,29 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.build_version macos, 14, 0	sdk_version 14, 0
+	.globl	_main                           ; -- Begin function main
+	.p2align	2
+_main:                                  ; @main
+	.cfi_startproc
+; %bb.0:
+	stp	x29, x30, [sp, #-16]!           ; 16-byte Folded Spill
+	.cfi_def_cfa_offset 16
+	mov	x29, sp
+	.cfi_def_cfa w29, 16
+	.cfi_offset w30, -8
+	.cfi_offset w29, -16
+Lloh0:
+	adrp	x0, l_str@PAGE
+Lloh1:
+	add	x0, x0, l_str@PAGEOFF
+	bl	_puts
+	mov	w0, #0
+	ldp	x29, x30, [sp], #16             ; 16-byte Folded Reload
+	ret
+	.loh AdrpAdd	Lloh0, Lloh1
+	.cfi_endproc
+                                        ; -- End function
+	.section	__TEXT,__cstring,cstring_literals
+l_str:                                  ; @str
+	.asciz	"Hello world!"
+
+.subsections_via_symbols
